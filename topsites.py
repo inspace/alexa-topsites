04ea1216d9b768f1e1b1ac14bc2858c84b2de403
Matthew Calder <matt@Matthews-MacBook-Pro.local>
2014-01-07 00:04:34 -0800

    clean up

--- topsites.py
+++ topsites.py
@@ -1,25 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 from bs4 import BeautifulSoup
 import requests
 import sys
-import math
-
-"""
-http://en.wikipedia.org/wiki/ISO_3166-1
-"""
+from math import ceil
 
 BASE_URL='http://www.alexa.com/topsites/countries;%d/%s'
 
 if __name__ == '__main__':
     
     if len(sys.argv) != 3:
-        sys.stderr.write('Usage: country-code(ISP 3166-1)] top-n\n')
+        sys.stderr.write('Usage: COUNTRY-CODE TOP-N\n')
         sys.exit(1)
     
     country_code = sys.argv[1].upper()
     number = int(sys.argv[2])
+    delimiter = ' '
 
-    page_numbers = int(math.ceil(number/25.0))
+    page_numbers = int(ceil(number/25.0))
 
     for page_num in range(0, page_numbers): 
         response = requests.get(BASE_URL % (page_num, country_code))  
@@ -30,4 +27,4 @@ if __name__ == '__main__':
         for bullet in bullets:
             rank = bullet.div.contents[0]
             site = bullet.h2.a.contents[0]
-            print('%s %s' % (rank, site))
+            print('%s%s%s' % (rank, delimiter, site))
